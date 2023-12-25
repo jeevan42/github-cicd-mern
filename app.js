b2f49b065094874a3c8d935b8e7a0372dd347008
jeevan42 <g1kumar05092002@gmail.com>
2023-12-25 11:18:00 +0530

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/fronend/build/')));
+
+app.use(
+    cors({
+      methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+      origin: function (origin, callback) {
+        // Allow multiple origins (you can whitelist your domains here)
+        const whitelist = [
+          "http://localhost:3000",
+        ];
+        if (whitelist.includes(origin) || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      credentials: true, // Important for cookies
+    })
+  );
 
 app.get('/names', (req, res) => {
     res.send('jeevan');
